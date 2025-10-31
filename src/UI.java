ecd20b4f624cea963bcfe0167caad70144df0bfd
Davide <davidemeucci2007@gmail.com>
2025-10-31 12:17:51 +0100

    Add UI

--- src/UI.java
+++ src/UI.java
@@ -1,12 +1,36 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
 public class UI extends JFrame {
+    private JPanel panel, buttonPanel, inputPanel;
+    private JButton encryptButton, decryptButton;
+    private JButton fileDialogButton;
+    private JLabel algorithmLabel, keyLabel, ivLabel;
+    private JComboBox<String> algorithmComboBox;
+    private JTextField keyTextField, ivTextField, directoryTextField;
 
     public UI() {
         setTitle("Crypto Loacker");
-        setSize(400, 300);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        setSize(400, 200);
+
+        panel = new JPanel(new GridLayout(2, 1));
+        add(panel);
+
+        fileDialogButton = new JButton("CHOOSE DIRECTORY");
+        directoryTextField = new JTextField();
+        directoryTextField.setEditable(false);
+
+        algorithmLabel = new JLabel("Select algorithm:");
+        algorithmComboBox = new JComboBox<>(new String[]{"DES-ECB", "DES-CBC", "AES-256-ECB", "AES-256-CBC"});
+
+        keyLabel = new JLabel("Enter the key:");
+        keyTextField = new JTextField(20);
+
+        ivLabel = new JLabel("Enter the IV:");
+        ivTextField = new JTextField(20);
     }
 
     public static void main(String[] args) {
